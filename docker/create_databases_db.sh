4d91e4f0f119affffe95b084709df2efc211c2c5
Renan Reis <renan.reis@locaweb.com.br>
2023-11-23 14:41:40 -0300

    refactor generals

--- docker/create_databases_db.sh
+++ docker/create_databases_db.sh
@@ -2,5 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  # CREATE DATABASE new-database;
+  CREATE DATABASE ms_central_salary;
+  CREATE DATABASE ms_central_benefits;
 EOSQL
